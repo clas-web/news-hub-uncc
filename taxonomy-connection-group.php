6c5d7387a82e2cd3e4dea11ab49319a7cf64c508
Crystal Barton <cbarto11@uncc.edu>
2014-03-26 08:31:01 -0400

    add in custom listing-name and description for listing pages.

--- taxonomy-connection-group.php
+++ taxonomy-connection-group.php
@@ -6,13 +6,20 @@
  * @subpackage clas-exchange
  */
 
-ns_print('page:taxonomy-connection-group.php');
+//ns_print('page:taxonomy-connection-group.php');
 global $ns_config, $ns_template_vars;
 
 $ns_template_vars = array();
 $ns_template_vars['content-type'] = 'listing';
 $ns_template_vars['page-title'] = single_term_title( '', false );
 $ns_template_vars['section'] = $ns_config->get_section( 'connection' );
+
+$settings = Connections_ConnectionCustomPostType::get_settings();
+$ns_template_vars['listing-name'] = $settings['name']['group']['full_single'];
+$ns_template_vars['description'] = '';
+
+global $ns_clas_search_term;
+$ns_clas_search_term = $ns_template_vars['page-title'];
 
 ns_get_template_part( 'standard-template' );
 
--- taxonomy-connection-link.php
+++ taxonomy-connection-link.php
@@ -6,13 +6,21 @@
  * @subpackage clas-exchange
  */
 
-ns_print('page:taxonomy-connection-link.php');
+//ns_print('page:taxonomy-connection-link.php');
 global $ns_config, $ns_template_vars;
 
 $ns_template_vars = array();
 $ns_template_vars['content-type'] = 'listing';
 $ns_template_vars['page-title'] = single_term_title( '', false );
 $ns_template_vars['section'] = $ns_config->get_section( 'connection' );
+
+$settings = Connections_ConnectionCustomPostType::get_settings();
+$ns_template_vars['listing-name'] = $settings['name']['link']['full_single'];
+//$ns_template_vars['description'] = 'Click <a href="'.get_search_link( single_term_title( '', false ) ).'">here</a> to perform a search.';
+$ns_template_vars['description'] = '';
+
+global $ns_clas_search_term;
+$ns_clas_search_term = $ns_template_vars['page-title'];
 
 ns_get_template_part( 'standard-template' );
 
