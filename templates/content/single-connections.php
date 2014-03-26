fed5aad9cfb26861b3fb275d96384345dbab489c
Crystal Barton <cbarto11@uncc.edu>
2014-03-26 08:32:02 -0400

    fix bug in story template files that occurs when displaying search data.

--- templates/content/single-connections.php
+++ templates/content/single-connections.php
@@ -3,7 +3,6 @@
 <?php global $ns_config, $ns_mobile_support, $ns_template_vars, $post; ?>
 <?php
 $section = $ns_template_vars['section'];
-//ns_print( $section->key );
 $story = $section->get_single_story( $post );
 
 $links = $story['links'];
@@ -28,18 +27,20 @@ else
 	
 	<div class="connection-groups">
 		<?php foreach( $story['groups'] as $group ): ?>
-		<div><?php echo ns_get_anchor( $group['link'], $group['name'], null, $group['name'] ); ?></div>
+		<div><?php echo ns_get_anchor( $group['link'], $group['class'], null, $group['name'] ); ?></div>
 		<?php endforeach; ?>
 	</div><!-- .connection-groups -->
 	
-	<div class="details">
+	<div class="details clearfix">
 	
 		<div class="column column-1">
 		
 			<div class="links">
 				<?php echo ns_get_anchor( $story['link'], 'View Summary', 'view-summary', 'Summary' ); ?>
+				<?php if( $story['site-link'] !== null ): ?>
 				|
 				<?php echo ns_get_anchor( $story['site-link'], 'View Full Profile', 'view-full-profile', 'Full Profile' ); ?>
+				<?php endif; ?>
 			</div><!-- .links -->
 			
 			<div class="contact-info">
@@ -50,11 +51,15 @@ else
 		
 		<div class="column column-2">
 		
-			<div class="connection-links clearfix">
+			<?php $count = 1; ?>
+			<div class="connection-links columns-<?php echo count($links); ?> clearfix">
 				<?php foreach( $links as $link_column ): ?>
+				<div class="column column-<?php echo $count; ?>">
 				<?php foreach( $link_column as $link ): ?>
-				<div><?php echo ns_get_anchor( $link['link'], $link['name'], null, $link['name'] ); ?></div>
+				<div><?php echo ns_get_anchor( $link['link'], $link['class'], null, $link['name'] ); ?></div>
 				<?php endforeach; ?>
+				</div>
+				<?php $count++; ?>
 				<?php endforeach; ?>
 			</div><!-- .connection-links -->
 		
@@ -62,7 +67,7 @@ else
 	
 	</div><!-- .details -->
 	
-	<div class="post_contents">
+	<div class="post-contents">
 	
 		<?php echo $story['description']['text']; ?>
 	
--- templates/story/listing-connections.php
+++ templates/story/listing-connections.php
@@ -27,18 +27,20 @@ else
 	
 	<div class="connection-groups">
 		<?php foreach( $story['groups'] as $group ): ?>
-		<div><?php echo ns_get_anchor( $group['link'], $group['name'], null, $group['name'] ); ?></div>
+		<div><?php echo ns_get_anchor( $group['link'], $group['class'], null, $group['name'] ); ?></div>
 		<?php endforeach; ?>
 	</div><!-- .connection-groups -->
 	
-	<div class="details">
+	<div class="details clearfix">
 	
 		<div class="column column-1">
 		
 			<div class="links">
 				<?php echo ns_get_anchor( $story['link'], 'View Summary', 'view-summary', 'Summary' ); ?>
+				<?php if( $story['site-link'] !== null ): ?>
 				|
 				<?php echo ns_get_anchor( $story['site-link'], 'View Full Profile', 'view-full-profile', 'Full Profile' ); ?>
+				<?php endif; ?>
 			</div><!-- .links -->
 			
 			<div class="contact-info">
@@ -49,17 +51,25 @@ else
 		
 		<div class="column column-2">
 		
-			<div class="connection-links clearfix">
+			<?php $count = 1; ?>
+			<div class="connection-links columns-<?php echo count($links); ?> clearfix">
 				<?php foreach( $links as $link_column ): ?>
+				<div class="column column-<?php echo $count; ?>">
 				<?php foreach( $link_column as $link ): ?>
-				<div><?php echo ns_get_anchor( $link['link'], $link['name'], null, $link['name'] ); ?></div>
+				<div><?php echo ns_get_anchor( $link['link'], $link['class'], null, $link['name'] ); ?></div>
 				<?php endforeach; ?>
+				</div>
+				<?php $count++; ?>
 				<?php endforeach; ?>
 			</div><!-- .connection-links -->
 		
 		</div><!-- .column-2 -->
-	
+		
 	</div><!-- .details -->
+
+	<?php if( isset( $story['search-content'] ) ): ?>
+		<div class="search-content"><?php echo $story['search-content']; ?></div>
+	<?php endif; ?>	
 	
 </a>
 </div><!-- .story -->
