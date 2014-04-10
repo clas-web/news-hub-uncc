c6e73a55ca1d6a5c519462509696c6b26ca7e90e
Crystal Barton <cbarto11@uncc.edu>
2014-04-10 02:54:07 -0400

    add and style title-box options.

--- templates/default/header.php
+++ templates/default/header.php
@@ -4,10 +4,11 @@
 <?php if( $ns_config->show_template_part('header') ): ?>
 
 <?php 
-//ns_print($ns_config->get_value('header', 'header-wrapper-bg', 'url'));
-//ns_print(ns_get_image_url($ns_config->get_value('header', 'header-wrapper-bg', 'url')));
 $header_wrapper_bg = ns_get_image_url( $ns_config->get_value('header', 'header-wrapper-bg', 'url') );
 $header_bg = ns_get_image_url( $ns_config->get_value('header', 'header-bg', 'url') );
+
+$title = $ns_config->get_value('header', 'title');
+$description = $ns_config->get_value('header', 'description');
 ?>
 
 <div id="header-wrapper" class="clearfix" style="background-image:url('<?php echo $header_wrapper_bg; ?>');">
@@ -15,10 +16,9 @@ $header_bg = ns_get_image_url( $ns_config->get_value('header', 'header-bg', 'url
 	<div id="header" class="clearfix">
 	<?php ns_use_widget( 'header', 'top' ); ?>
 
-	
 	<div class="masthead" style="background-image:url('<?php echo $header_bg; ?>');">
 	
-		<?php ns_image( $ns_config->get_value('header', 'uncc-logo') ); ?>
+		<?php ns_image( $ns_config->get_value('header', 'logo') ); ?>
 
 		<?php if( !$ns_mobile_support->use_mobile_site ): ?>
 			
@@ -26,16 +26,11 @@ $header_bg = ns_get_image_url( $ns_config->get_value('header', 'header-bg', 'url
 			$title_box_info = $ns_config->get_value('header', 'title-box'); 
 			?>
 			<div class="title-box-wrapper" style="height:100px">
-			<div class="title-box <?php echo $title_box_info['position'] ?>">
-				<a href="<?php echo $title_box_info['title-link']; ?>">
-				<div class="name"><?php echo $title_box_info['title']; ?></div>
-				</a>
+			<div class="title-box <?php echo $title['position']; ?>">
+				<?php echo ns_get_anchor( $title['url'], $title['text'], 'title', '<div>'.$title['text'].'</div>' ); ?>
+				<?php echo ns_get_anchor( $description['url'], $description['text'], 'description', '<div>'.$description['text'].'</div>' ); ?>
 			</div>
 			</div>
-		
-			<?php if( !empty($image_info['link']) ): ?>
-				<a href="<?php echo $image_info['link']; ?>" title="<?php echo $image_info['title']; ?>" class="click-box"></a>
-			<?php endif; ?>
 		
 			<div id="links">
 				<a href="<?php echo home_url( '/' ); ?>" title="">Home</a>
@@ -44,7 +39,7 @@ $header_bg = ns_get_image_url( $ns_config->get_value('header', 'header-bg', 'url
 			<div id="header-utility">
 				<form id="site-searchform" class="searchform" method="get" action="<?php echo home_url( '/' ); ?>">
 					<script>var main_search_used = false;</script>
-					<input type="text" name="s" id="s" size="30" value="<?php if( is_search() ) { the_search_query(); } else { echo "Search ".get_bloginfo('name'); } ?>" onfocus="if (!main_search_used) { this.value = ''; main_search_used = true; }" /><input type="image" name="op" value="Search" id="edit-submit" alt="search" title="Search this site" src="<?php print get_stylesheet_directory_uri() ?>/images/search-button.png">
+					<input type="text" name="s" id="header-search" class="s" size="30" value="<?php if( is_search() ) { the_search_query(); } else { echo "Search ".get_bloginfo('name'); } ?>" onfocus="if (!main_search_used) { this.value = ''; main_search_used = true; }" /><input type="image" name="op" value="Search" id="edit-submit" alt="search" title="Search this site" src="<?php print get_stylesheet_directory_uri() ?>/images/search-button.png">
 				</form>
 			</div><!-- #header-utility -->
 			
