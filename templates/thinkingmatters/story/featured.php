ef4a90e484b0d9867f36888c7aff5c42b8a67187
Crystal Barton <cbarto11@uncc.edu>
2014-04-09 23:52:39 -0400

    alter css.

--- templates/thinkingmatters/story/featured.php
+++ templates/thinkingmatters/story/featured.php
@@ -7,31 +7,33 @@ $story = $ns_template_vars['story'];
 ?>
 
 <div class="story <?php echo $section->key; ?>-section <?php echo $section->thumbnail_image; ?>-image clearfix">
-<?php echo ns_get_anchor( $story['link'], $story['title'] ); ?>
-
-	<?php if( $section->thumbnail_image !== 'none' ): ?>
-	
-		<div class="image">
-		
-			<?php if( $story['image'] ): ?>
-				<img src="<?php echo $story['image']; ?>" alt="Featured Image" />
-			<?php endif; ?>
-			
-			<?php if( $story['embed'] ): ?>
-				<?php echo $story['embed']; ?>
-			<?php endif; ?>
-
-		</div><!-- .image -->
-	
-	<?php endif; ?>
 
 	<div class="details">
+
+		<?php if( $section->thumbnail_image !== 'none' ): ?>
 	
-		<h3><?php echo $story['title']; ?></h3>
+			<div class="image">
 		
+				<?php if( $story['image'] ): ?>
+					<img src="<?php echo $story['image']; ?>" alt="Featured Image" />
+				<?php endif; ?>
+			
+				<?php if( $story['embed'] ): ?>
+					<?php echo $story['embed']; ?>
+				<?php endif; ?>
+
+			</div><!-- .image -->
+	
+		<?php endif; ?>
+	
+		<h3><?php echo ns_get_anchor( $story['link'], $story['title'], null, $story['title'] ); ?></h3>
+		
+		<div class="byline"><?php echo $story['byline']; ?></div>
+				
 		<?php if( count($story['description']) > 0 ): ?>
 
 			<div class="description">
+			<?php echo ns_get_anchor( $story['link'], $story['title'] ); ?>
 
 			<?php 
 			foreach( $story['description'] as $key => $value ):
@@ -53,12 +55,12 @@ $story = $ns_template_vars['story'];
 			endforeach;
 			?>
 	
-			</div><!-- .contents -->
+			</a>
+			</div><!-- .description -->
 
-		<?php endif; ?>
+		<?php endif; ?>		
 	
-	</div><!-- .description -->
+	</div><!-- .details -->
 	
-</a>
 </div><!-- .story -->
 
