da1f7358984106de61498c18e7fb177a661880f4
Crystal Barton <cbarto11@uncc.edu>
2014-04-09 23:54:03 -0400

    update thinkingmatters description data.

--- functions/thinkingmatters.php
+++ functions/thinkingmatters.php
@@ -5,55 +5,23 @@ add_filter( 'ns-listing-story', 'thinkingmatters_get_listing_story', 99, 2 );
 add_filter( 'ns-single-story', 'thinkingmatters_get_single_story', 99, 2 );
 
 
-
-
-// 	global $post;
-// 
-// 	$src = get_post_meta($post->ID, 'thumbnail', TRUE);
-// 	if( $src !== '' )
-// 		$excerpt = '<div class="fwp_uncc_thumbnail"><img src="'.$src.'" /></div><div class="fwp_uncc_excerpt">'.$excerpt."</div>";
-// 
-// 	if( has_excerpt() )
-// 		return $excerpt.'<a href="'.get_permalink().'" class="continue-reading">'.__( 'More &rarr;', '2010-translucence' ).'</a>';;
-// 		
-// 	if( strlen($output) < strlen($post->post_content) )
-// 		return $excerpt.'&hellip; <a href="'.get_permalink().'" class="continue-reading">'.__( 'More &rarr;', '2010-translucence' ).'</a>';
-// 
-// 	return $excerpt;
-
-
-function thinkingmatters_get_featured_story(  $story, $post )
+function thinkingmatters_get_featured_story( $story, $post )
 {
-	$story['image'] = thinkingmatters_get_image( $post->ID );
-	$story['description']['post_date'] = $post->post_modified;
-	$story['description']['attribution_directory'] = 'attribution directory';
-	
-	uksort( $story['description'], 'thinkingmatters_sort_description' );
-	
+	thinkingmatters_get_story_data( $story, $post );
 	return $story;
 }
 
 
-function thinkingmatters_get_listing_story(  $story, $post )
+function thinkingmatters_get_listing_story( $story, $post )
 {
-	$story['image'] = thinkingmatters_get_image( $post->ID );
-	$story['description']['post_date'] = $post->post_modified;
-	$story['description']['attribution_directory'] = 'attribution directory';
-	
-	uksort( $story['description'], 'thinkingmatters_sort_description' );
-	
+	thinkingmatters_get_story_data( $story, $post );
 	return $story;
 }
 
 
-function thinkingmatters_get_single_story(  $story, $post )
+function thinkingmatters_get_single_story( $story, $post )
 {
-	$story['image'] = thinkingmatters_get_image( $post->ID );
-	$story['description']['post_date'] = $post->post_modified;
-	$story['description']['attribution_directory'] = 'attribution directory';
-	
-	uksort( $story['description'], 'thinkingmatters_sort_description' );
-	
+	thinkingmatters_get_story_data( $story, $post );
 	return $story;
 }
 
@@ -69,12 +37,25 @@ function thinkingmatters_get_image( $post_id )
 function thinkingmatters_sort_description( $a, $b )
 {
 	$array = array( $a => -1, $b => 1 );
-	if( array_key_exists('post_date', $array) )
-		return $array['post_date'];
-	if( array_key_exists('attribution_directory', $array) )
-		return $array['attribution_directory'];
+	if( array_key_exists('by_line', $array) ) return $array['by_line'];
 	return 0;
 }
 
+function thinkingmatters_get_story_data( &$story, $post )
+{
+	$story['image'] = thinkingmatters_get_image( $post->ID );
+	$story['byline'] = thinkingmatters_get_byline( $post );
+	
+	uksort( $story['description'], 'thinkingmatters_sort_description' );
+}
 
+function thinkingmatters_get_byline( $post )
+{
+	$date = date( 'F d, Y', strtotime($post->post_modified) );
+	$author = get_the_author_meta( 'display_name', $post->post_author );
+	$url = get_the_author_meta( 'user_url', $post->post_author );
+	
+//	return $date.' by '.$author;
+	return $date.' by <a href="'.$url.'" title="Posts by '.$author.'">'.$author.'</a>';
+}
 
